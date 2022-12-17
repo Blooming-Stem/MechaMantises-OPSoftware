a4aa9d5e2f62f79d9382905d7634551b3439f26d
neilk <mechamantises@gmail.com>
2022-12-16 19:14:47 -0800

    llksgu

---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -0,0 +1,47 @@
+package com.example.meepmeep;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+public class MeepMeepTesting {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(800);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(52.48, 52.48, Math.toRadians(281.5), Math.toRadians(222.7), 13.56)
+                .followTrajectorySequence(drive ->
+
+
+                        drive.trajectorySequenceBuilder(new Pose2d(-35,60,Math.toRadians(90)))
+                                .lineTo(new Vector2d(-35,22))
+                                // Drop cone above
+                                .lineTo(new Vector2d(-35, 0))
+
+                                .build()
+
+                );
+
+
+
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_OFFICIAL)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
+}
+
+
+
+
+
+
+
+
+
+
